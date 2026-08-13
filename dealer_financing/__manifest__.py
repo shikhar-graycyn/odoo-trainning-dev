c0ed8630c252e0a2927d8fbfb580f8b6d7bf07d3
shikhar_gc <shikhar_gc@U-3IT7A4EF616FI.workspaces.graycyan.com>
2026-08-13 09:34:31 +0530

    feat: New app dealer financing

---
+++ dealer_financing/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Dealer Financing",
+    "summary": "Streamlines the financing application process for dealerships.",
+    "category": "Finance/Custom Modules",
+    "version": "1.0.0",
+    "author": "ODOP Trainee",
+    "maintainer": "Shikhar",
+    "depends": ["base"],
+    "license": "OPL-1",
+    "application": True,
+}
